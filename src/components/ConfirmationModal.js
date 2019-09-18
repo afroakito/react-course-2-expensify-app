1bc201d695974f57bbf496fb8adf5a6cd846cd26
アキト <akitowens999916@gmail.com>
2019-09-18 10:25:43 +0900

    Style confirmation modal and display number of hidden

--- src/components/ConfirmationModal.js
+++ src/components/ConfirmationModal.js
@@ -6,13 +6,16 @@ Modal.setAppElement('#app');
 const ConfirmationModal = (props) => (
     <Modal
         isOpen={props.confModal}
-
+        onRequestClose={props.handleClearModal}
         contentLabel='Confirmation'
         closeTimeoutMS={200}
+        className="modal"
     >
         <h3 className="modal__title">Do you actually want to delete this expense?</h3>
-        <button onClick={props.handleAcceptModal}>Okay</button>
-        <button onClick={props.handleClearModal}>No</button>
+        <div className="modal__button-wrapper">
+            <button className="button accept" onClick={props.handleAcceptModal}>Okay</button>
+            <button className="button reject" onClick={props.handleClearModal}>No</button>
+        </div>
     </Modal>
 );
 
--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -5,7 +5,7 @@ import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 
-export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+export const ExpensesSummary = ({ expenseTotalCount, expenseCount, expensesTotal }) => {
     const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
     const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00')
 
@@ -13,6 +13,7 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
             <div className="page-header">
                 <div className="content-container">
                     <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal}</span></h1>
+                    <h2 className="page-header__title">Hidden: <span>{expenseTotalCount - expenseCount}</span></h2>
                     <div className="page-header__actions">
                         <Link className="button" to="/create">Add Expense</Link>
                     </div>
@@ -25,6 +26,7 @@ const mapStateToProps = (state) => {
     const visibleExpenses = selectExpenses(state.expenses, state.filters);
 
     return {
+        expenseTotalCount: state.expenses.length,
         expenseCount: visibleExpenses.length,
         expensesTotal: selectExpensesTotal(visibleExpenses)
     };
